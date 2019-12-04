7d8b1faefcd76d2f94a1502296c466f0677f56e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-04 18:52:33 +0000

    Deploy KotAleksei/js-band-hw-task-7 to github.com/KotAleksei/js-band-hw-task-7.git:gh-pages

---
+++ precache-manifest.e32bbda9cd836ac584e71b2142816846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91c43506c042dfc44d222de828d20b4",
+    "url": "/js-band-hw-task-7/index.html"
+  },
+  {
+    "revision": "0558597c65e0b2a1687e",
+    "url": "/js-band-hw-task-7/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "69298a72353e6a206d80",
+    "url": "/js-band-hw-task-7/static/css/main.e0486ae0.chunk.css"
+  },
+  {
+    "revision": "0558597c65e0b2a1687e",
+    "url": "/js-band-hw-task-7/static/js/2.0cda4569.chunk.js"
+  },
+  {
+    "revision": "69298a72353e6a206d80",
+    "url": "/js-band-hw-task-7/static/js/main.db1eb11c.chunk.js"
+  },
+  {
+    "revision": "97a2c6dac15973b5b488",
+    "url": "/js-band-hw-task-7/static/js/runtime-main.3f21594c.js"
+  }
+]);
